c2991d9925bd0e1eec849fcab5d2452f24873346
Alfredo AD <alfredo7891@gmail.com>
2014-03-21 14:46:10 -0500

    Shaking Things Up
    adding-a-shake-detector

--- src/com/example/boladecristal/MainActivity.java
+++ src/com/example/boladecristal/MainActivity.java
@@ -2,23 +2,27 @@ package com.example.boladecristal;
 
 import android.app.Activity;
 import android.graphics.drawable.AnimationDrawable;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.media.MediaPlayer;
 import android.media.MediaPlayer.OnCompletionListener;
 import android.os.Bundle;
 import android.view.Menu;
-import android.view.View;
 import android.view.animation.AlphaAnimation;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.example.boladecristal.ShakeDetector.OnShakeListener;
 // CTRL + SHIFT + O = actualiza lo imports 
 
 public class MainActivity extends Activity {
 	
 	private BolaDeCristal mBoladeCristal = new BolaDeCristal();
 	private TextView mRespuestaLabel;
-	private Button mGetRespuetaBoton;
 	private ImageView mBoladeCristalImagen;
+	private SensorManager mSensorManager;
+	private Sensor mAccelerometer;
+	private ShakeDetector mShakeDetector;
 	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,22 +31,15 @@ public class MainActivity extends Activity {
         
         //Declarando variables
         mRespuestaLabel = (TextView) findViewById(R.id.textView1);
-        mGetRespuetaBoton = (Button) findViewById(R.id.button1);
         mBoladeCristalImagen = (ImageView) findViewById(R.id.imageView1);
         
-        mGetRespuetaBoton.setOnClickListener(new View.OnClickListener() {
+        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mShakeDetector = new ShakeDetector(new OnShakeListener() {
 			
 			@Override
-			public void onClick(View v) {
-			
-				String respuesta = mBoladeCristal.getUnaRespuesta();
-				
-				//Actualizar el nuevo valor aleatorio en el label			
-				mRespuestaLabel.setText(respuesta);
-			
-				animarBolaDeCristal();
-				animarRespuesta();
-				playSonido();
+			public void onShake() {
+				manejarNuevaRespuesta();
 			}
 		});
     }
@@ -80,5 +77,16 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
+
+	private void manejarNuevaRespuesta() {
+		String respuesta = mBoladeCristal.getUnaRespuesta();
+		
+		//Actualizar el nuevo valor aleatorio en el label			
+		mRespuestaLabel.setText(respuesta);
+
+		animarBolaDeCristal();
+		animarRespuesta();
+		playSonido();
+	}
     
 }
